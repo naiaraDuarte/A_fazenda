490d6dbc65a95564dc5d0cee9c7c0e326f476b39
Alexandre Lucas <lucascunha10x@gmail.com>
2020-07-11 23:13:01 -0300

    refatoração do apagar.php

--- sistema/apagar.php
+++ sistema/apagar.php
@@ -2,13 +2,17 @@
     include 'database.php';
     $tipo = filter_input(INPUT_GET, 'tipo', FILTER_SANITIZE_STRING);
     $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
-    if($tipo == 'doca') {
-        echo 'dados recebidos';
-        echo 'id cargo:'. $id;
+
+
+    if($tipo == 'tb_coleta' || $tipo == 'tb_rebanho' || $tipo == 'tb_gado' || $tipo == 'tb_fazendeiro' || $tipo == 'tb_gado_leiteiro' || $tipo == 'tb_criador'){
+        $sql = "DELETE FROM `$tipo` WHERE `CD_CODIGO` = $id";
+        execute($sql);
+    } else {
+        die('Tipo inválido');
     }
-        $sql = "delete from `$tipo` where CD_CODIGO = $id";
-        header('Location: /vaca/coletaLeite.php');
-    execute($sql);
-    echo '<script>console.log("Deletado com sucesso!")</script>';
+
+   
+    header('Location: coletaLeite.php');
+    
     exit;
 ?>
